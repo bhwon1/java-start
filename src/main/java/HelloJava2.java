f14944c2ed2b3b7d5dc32a0aeea39eae8bb0065b
BHW <hwbae.work@gmail.com>
2026-04-21 13:46:24 +0900

    주석 연습

---
+++ src/main/java/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello java 01");
+        System.out.println("hello java 02");
+        System.out.println("hello java 03");
+    }
+}
