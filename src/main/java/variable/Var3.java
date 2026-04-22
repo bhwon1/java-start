ad2e21a422199b0e5e2a45572261e4436d02b400
BHW <hwbae.work@gmail.com>
2026-04-22 14:20:44 +0900

    변수 선언

---
+++ src/main/java/variable/Var3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var3 {
+    static void main(String[] args) {
+        int a;
+        a = 10;
+        System.out.println(a);
+
+        a = 50;
+        System.out.println(a);
+
+
+    }
+}
