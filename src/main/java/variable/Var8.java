18529b1da48b3a9f546926edbe0a22ddd0654633
BHW <hwbae.work@gmail.com>
2026-04-23 17:13:10 +0900

    변수 타입 2

---
+++ src/main/java/variable/Var8.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var8 {
+    static void main(String[] args) {
+        // 정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; // -32767 ~ 32767
+        int i = 2147473838; // 약 20억
+
+        long l = 3893289423849832L; // 제일 큰
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+
+        // 결론: 사용하는것
+        // int, double, boolean, String 만 쓰면 됨
+    }
+}
